5b5c8a4b7eb87c591d0c67913091f6a4e81d150d
bala-amypo <bala@amypo.in>
2025-12-18 09:19:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -12,5 +12,29 @@ public class User{
     private String password;
     private String ADMIN;
     private String STAFF
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getADMIN() {
+        return ADMIN;
+    }
+    public String getSTAFF() {
+        return STAFF;
+    }
+    @coloumn(unique=tue)
+    private String email;
+    private String password;
+    private String ADMIN;
+    private String STAFF
     
 }
