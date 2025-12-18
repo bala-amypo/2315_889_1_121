85dbcb4c9a5568b541b2306ffa5cf7d449b12dfa
bala-amypo <bala@amypo.in>
2025-12-18 09:49:57 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,7 +1,30 @@
 package com.example.demo.entity;
 
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
 
 public class Student{
-    private Long
+    @Id
+    private Long id;
+    @Column(unique=true)
+    private String rollNumber;
+    private String name;
+    private String department;
+    private Integer year;
+    public Long getId() {
+        return id;
+    }
+    public String getRollNumber() {
+        return rollNumber;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getDepartment() {
+        return department;
+    }
+    public Integer getYear() {
+        return year;
+    }
+
 }
