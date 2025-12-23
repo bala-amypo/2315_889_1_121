b3a376a068adf87fec87ad6e37577c750c499356
bala-amypo <bala@amypo.in>
2025-12-23 05:38:31 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -63,23 +63,3 @@ public class User {
         return createdAt;
     }
 }
-repository
-ExamRoomRepository.java
-package com.example.demo.repository;
-
-import com.example.demo.model.ExamRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface ExamRoomRepository extends JpaRepository<ExamRoom, Long> {
-    Optional<ExamRoom> findByRoomNumber(String roomNumber);
-}
-ExamSessionRepository.java
-package com.example.demo.repository;
-
-import com.example.demo.model.ExamSession;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExamSessionRepository extends JpaRepository<ExamSession, Long> {
-}
