8ab0b32741cfcf9c86a65095d3e6d1dce3c29278
bala-amypo <bala@amypo.in>
2025-12-25 10:27:45 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ExamSessionService.java
+++
@@ -1,8 +0,0 @@
-// package com.example.demo.service;
-
-// import com.example.demo.model.ExamSession;
-
-// public interface ExamSessionService {
-//     ExamSession create(ExamSession session);
-//     ExamSession get(Long id);
-// }
--- src/main/java/com/example/demo/service/impl/StudentService.java
+++
@@ -1,9 +0,0 @@
-// package com.example.demo.service;
-
-// import com.example.demo.model.Student;
-// import java.util.List;
-
-// public interface StudentService {
-//     Student add(Student student);
-//     List<Student> all();
-// }
