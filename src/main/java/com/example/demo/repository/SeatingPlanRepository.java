e17d6cb87325a5a103db484b8edc49a9ee31a51f
bala-amypo <bala@amypo.in>
2025-12-25 11:03:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/SeatingPlanRepository.java
+++
@@ -1,7 +0,0 @@
-// package com.example.demo.repository;
-
-// import com.example.demo.model.SeatingPlan;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// public interface SeatingPlanRepository extends JpaRepository<SeatingPlan, Long> {
-// }
